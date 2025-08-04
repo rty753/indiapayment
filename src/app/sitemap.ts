dcd6a80a61e67c44e7b1fefa1c208b262334b39e
rty753 <vfrty753@gmail.com>
2025-08-04 19:59:39 +0800

    多/问题

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://indiapayment.org/'
+  const baseUrl = 'https://indiapayment.org'
 
   return [
     {
